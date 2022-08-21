a798c3c66de14191db8e1649411bc984c3bd0fa5
Abraham Duno <tresduno@gmail.com>
2022-08-21 11:18:31 -0400

    :+1:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { useState, GetStaticProps } from 'react'
+import { useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 
@@ -40,7 +40,7 @@ function BlurImage({ image }: { image: Image}) {
 
 export default Gallery
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps = async () => {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     process.env.SUPABASE_SERVICE_ROLE_KEY || '',
